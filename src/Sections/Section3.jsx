b1ffc89b8f5da30750646d549e7cc407e543e7a2
StudenxFinance <studenxfinance@gmail.com>
2024-06-25 17:06:50 +0300

    upt

--- src/Sections/Section3.jsx
+++ src/Sections/Section3.jsx
@@ -13,6 +13,13 @@ export const Section3 = () => {
         <div className="three-content">
           <div className="three-content-left">
             <h1 className="headline-purple split">Our Mission</h1>
+            <p className="description">
+              Our mission at DialedWeb is to innovate and enhance <br /> digital
+              experiences by crafting tailored, user-centric web<br /> solutions.
+              We elevate and engage audiences through <br /> creativity and
+              technology, delivering modern, intuitive <br /> websites that drive
+              business’s international success.
+            </p>
           </div>
           <div className="three-content-right">
               <div className="reveal">
