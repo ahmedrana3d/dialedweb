d2ff5f555fa526a1f3d282129e64bda5350e9f49
StudenxFinance <studenxfinance@gmail.com>
2024-06-27 16:18:22 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -107,7 +107,21 @@ function App() {
               <sphereGeometry />
             </mesh> */}
             <EffectComposer>
-              <Fluid showBackground={ false } intensity={ 0.35 } radius={ 0.05 } fluidColor="#a09bcb" />
+              <Fluid
+                    radius={0.03}
+                    curl={10}
+                    swirl={5}
+                    distortion={1}
+                    force={2}
+                    pressure={0.94}
+                    densityDissipation={0.98}
+                    velocityDissipation={0.99}
+                    intensity={0.3}
+                    rainbow={false}
+                    blend={0}
+                    showBackground={false}
+                    fluidColor='#a09bcb'
+                />
             </EffectComposer>
           </Canvas>
         </div>
