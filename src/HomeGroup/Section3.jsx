ba4c1e436bc6224465ce6a31d8515734922a7508
StudenxFinance <studenxfinance@gmail.com>
2024-07-05 18:31:57 +0300

    one try

--- src/HomeGroup/Section3.jsx
+++ src/HomeGroup/Section3.jsx
@@ -6,14 +6,14 @@ import { OrbitControls } from "@react-three/drei";
 
 export const Section3 = () => {
 
-  useAnimateText(".three-title")
+  // useAnimateText(".three-title")
 
-  const isMobile = window.innerWidth <= 768;
+  // const isMobile = window.innerWidth <= 768;
 
   return (
     <>
       <section className="three">
-        {!isMobile && (
+        {/* {!isMobile && (
           <div className="three-content-box-right">
             <div className="three-video-frame">
               <video className="three-video" src="/video.mp4" autoPlay="autoplay" muted="true" playsInline="true" data-wf-ignore="true" preload="auto" loop></video>
@@ -37,26 +37,8 @@ export const Section3 = () => {
               </div>
             </div>
           )}
-        </div>
+        </div> */}
       </section>
     </>
   );
 };
-
-{/* <div className="three-content-left">
-<h1 className="headline-purple split">Our Mission</h1>
-<p className="description">
-  Our mission at DialedWeb is to innovate and enhance <br /> digital
-  experiences by crafting tailored, user-centric web<br /> solutions.
-  We elevate and engage audiences through <br /> creativity and
-  technology, delivering modern, intuitive <br /> websites that drive
-  business’s international success.
-</p>
-</div>
-<div className="three-content-right">
-  <div className="reveal">
-    <div className="image-wrap">
-      <video src="/video.mp4" autoPlay="autoplay" muted="true" playsInline="true" data-wf-ignore="true" preload="auto" className="background-video" loop ></video>
-    </div>
-  </div>
-</div> */}
