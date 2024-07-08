97725ae585a99af514dfc9f02b915b463679b7b5
StudenxFinance <studenxfinance@gmail.com>
2024-07-08 17:47:07 +0300

    update

--- src/HomeGroup/SectionVideoOnScroll.jsx
+++ src/HomeGroup/SectionVideoOnScroll.jsx
@@ -72,7 +72,7 @@ export const SectionScrollVideoOnScroll = () => {
     return (
       <>
         <section className="scroll-video-on-scroll">
-            <video src="./videotry.mp4"       playsinline="true" webkit-playsinline="true" preload="auto"       muted="muted" class="video-background"></video>
+            <video src="./flower60.mp4"       playsinline="true" webkit-playsinline="true" preload="auto"       muted="muted" class="video-background"></video>
         </section>
       </>
     )
