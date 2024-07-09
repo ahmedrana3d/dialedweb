4b08e7fc278f6fd735c1a463f88cfae37e30c41e
StudenxFinance <studenxfinance@gmail.com>
2024-07-09 15:44:11 +0300

    update

--- src/HomeGroup/SectionVideoOnScroll.jsx
+++ src/HomeGroup/SectionVideoOnScroll.jsx
@@ -72,7 +72,22 @@ export const SectionScrollVideoOnScroll = () => {
     return (
       <>
         <section className="scroll-video-on-scroll">
-            <video src="./flowerkey2.mp4" preload="auto" playsInline="true" webkit-playsinline="true" muted="muted" class="video-background"></video>
+            <div className="scroll-video-on-scroll-items">
+                <div className="scroll-video-items-wrapper">
+                    <h1 className="scroll-section-headline" >A</h1>
+                    <div className="box-between-words-small box-between-words-image-1" />
+                    <h1 className="scroll-section-headline" >tiny</h1>
+                    <h1 className="scroll-section-headline" >mountain</h1>
+                    <h1 className="scroll-section-headline" >village</h1>
+                    <div className="box-between-words-small box-between-words-image-2" />
+                    <h1 className="scroll-section-headline" >where</h1>
+                    <h1 className="scroll-section-headline" >it</h1>
+                    <h1 className="scroll-section-headline" >all</h1>
+                    <h1 className="scroll-section-headline" >began...</h1>
+                    <div className="box-between-words-big box-between-words-image-3" />
+                </div>
+            </div>
+            <video src="./flowerkey3.mp4" preload="auto" playsInline="true" webkit-playsinline="true" muted="muted" class="video-background"></video>
         </section>
       </>
     )
