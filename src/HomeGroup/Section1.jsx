f13314322fdb9d46aa31a677cb59ca1d3b65458a
StudenxFinance <studenxfinance@gmail.com>
2024-07-12 11:12:18 +0300

    update

--- src/HomeGroup/Section1.jsx
+++ src/HomeGroup/Section1.jsx
@@ -75,7 +75,7 @@ const hoverSoundMobile = () => {
   return (
     <>
       <section className="one">
-      <div className="background-element-kodu" />
+      <div className="background-element" />
       <div className="background-element-grid" />
         <div className="one-content">
           <div className="one-content-left">
