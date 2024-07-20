8447f0e7547f78b06c9b25edaba76cb5c59c8917
StudenxFinance <studenxfinance@gmail.com>
2024-07-20 22:02:37 +0300

    update

--- src/HomeGroup/SectionVideoOnScroll.jsx
+++ src/HomeGroup/SectionVideoOnScroll.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
+import { useAnimateText } from "../ScrollAnimations";
 
 export const SectionScrollVideoOnScroll = () => {
     useEffect(() => {
@@ -9,7 +10,10 @@ export const SectionScrollVideoOnScroll = () => {
         script.onload = () => {
             new ScrollyVideo({
                 scrollyVideoContainer: "scrolly-video",
-                src: "https://scrollyvideo.js.org/goldengate.mp4"
+                src: "/flowerkey3.mp4",
+                transitionSpeed: 14,
+                frameThreshold: 0.01,
+                full: false,
             });
         };
         document.body.appendChild(script);
@@ -20,10 +24,12 @@ export const SectionScrollVideoOnScroll = () => {
         };
     }, []);
 
+    useAnimateText(".scroll-headline-1, .scroll-headline-2, .scroll-headline-3, .scroll-headline-4, .scroll-headline-5, .scroll-headline-6, .scroll-headline-7, .scroll-headline-8, .box-between-words-image-1, .box-between-words-image-2, .box-between-words-image-3")
+
     return (
         <>
             <section className="scroll-video-on-scroll hover-area" data-cursor-text="SCROLL">
-                {/* <div className="scroll-video-on-scroll-items">
+                <div className="scroll-video-on-scroll-items">
                     <div className="scroll-video-items-wrapper">
                         <h1 className="scroll-section-headline scroll-headline-1">Grow</h1>
                         <h1 className="scroll-section-headline scroll-headline-2">your</h1>
@@ -36,8 +42,8 @@ export const SectionScrollVideoOnScroll = () => {
                         <motion.div whileHover={{ scale: 2, rotate: -10 }} transition={{ type: "tween", stiffness: 400, damping: 10 }} className="box-between-words-big box-between-words-image-3" />
                         <h1 className="scroll-section-headline scroll-headline-8">Bloom</h1>
                     </div>
-                </div> */}
-                <div id="scrolly-video"></div>
+                </div>
+                <div id="scrolly-video" className="video-background"></div>
             </section>
         </>
     )
